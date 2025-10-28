5113a31195bd9c75651ab1827b31fca66fddc7b7
nicolelinh444 <nicolelinh444@gmail.com>
2025-10-28 13:00:44 -0700

    added code to leap-year.js to determine if user input is a leap year

--- leap-year/leap-year.js
+++ leap-year/leap-year.js
@@ -5,3 +5,18 @@ Make an app that determines if a given year is a leap year!
 
 Read the instructions in the README.md file in this folder.
 ------------------------------ */
+
+// CommonJS Syntax
+// const moment = require("moment");
+
+// ES Module Syntax
+import moment from "moment";
+
+// declare variable for user input (year)
+const userYear = process.argv[2];
+
+// check if userYear is a leap year, will return boolean
+const checkUserYear = moment([userYear]).isLeapYear();
+
+// log a message stating if userYear is a leap year
+console.log(`Is ${userYear} a leap year?`, checkUserYear);
